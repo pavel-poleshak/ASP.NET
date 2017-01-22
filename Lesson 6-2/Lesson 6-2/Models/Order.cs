b89882559223d185a2354f11eddcdc4178608b84
pavel-poleshak <mr.gorden.beat@gmail.com>
2017-01-22 23:54:13 +0300

    Class Order

---
+++ Lesson 6-2/Lesson 6-2/Models/Order.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Lesson_6_2.Models
+{
+    public class Order
+    {
+       public IEnumerable<Product> ProductList { get; set; }
+        public int ProductsCount {
+            get
+            {
+                return ProductList.Count();
+            }
+        }
+
+    }
+}
